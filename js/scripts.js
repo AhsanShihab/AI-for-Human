ce7616875b7f90f39d33eab56efb6a023a37e6ce
Nazmul Ahsan <ahsan.shihab2@gmail.com>
2020-05-15 02:08:38 +0600

    Update Projects

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+function donationReceiveMessage() {
+    document.getElementById("donationReceive").innerHTML="Received! Thanks for supporting us!"
+    document.getElementById("donationReceive").style.color= "green"
+}
