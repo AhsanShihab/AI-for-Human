c3d9232b35c5126a4db7b8cac597ebd417cf5833
Nazmul Ahsan <ahsan.shihab2@gmail.com>
2020-05-15 12:48:53 +0600

    Publish to GitHub page

--- js/scripts.js
+++
@@ -1,4 +0,0 @@
-function donationReceiveMessage() {
-    document.getElementById("donationReceive").innerHTML="Received! Thanks for supporting us!"
-    document.getElementById("donationReceive").style.color= "green"
-}
